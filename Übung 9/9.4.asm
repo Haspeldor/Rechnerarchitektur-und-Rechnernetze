7f80bb89f5ad3fde27dbb6a24f200230dc8f5da8
Haspeldor <Haspeldor@gmail.com>
2019-12-17 12:03:13 +0100

    Übung 9

--- Übung 9/9.4.asm
+++ Übung 9/9.4.asm
@@ -37,40 +37,69 @@ pow1:
 
     addi $t1, $zero, 1
 
-    bne $a1, $zero, calculate_pow   # wenn exponent gleich 0, gib 1 zurück
-    move $v0, $t1
-    jr $ra
-
-    bne $a1, $zero, calculate_pow   # wenn exponent gleich 1, gib basis zurück                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+    bne $a1, $t1, check_zero_pow   # wenn exponent gleich 1, gib basis zurÃ¼ck
     move $v0, $a0
     jr $ra
 
+    check_zero_pow:
+    
+    bne $a1, $zero, calculate_pow   # wenn exponent gleich 0, gib 1 zurÃ¼ck                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+    move $v0, $t1
+    jr $ra
+
     calculate_pow:
+    
+    addi $sp, $sp, -12            # Stack fuer 2 Eintraege reservieren
+    sw $ra, 8($sp)		  # $ra,
+    sw $a1, 4($sp)		  # $a1,
+    sw $a0, 0($sp)                # $a0 sichern
+    
+    move $a1, $a0		# setze a1 auf a0
+    
+    jal multiply		  # errechne basis hoch 2
+    
+    lw $a0, 0($sp)                # $a0,
+    lw $a1, 4($sp)               # $a1 ,
+    lw $ra, 8($sp)		  # $ra wieder laden
+    addi $sp, $sp, 12             # stack wiederherstellen
+    
+    move $t4, $v0		# setze t4 auf ergebnis basis mal basis
 
-    mult $a0, $a0
-    mflo $a0
-    slr $a1, $a1, 1
-
-    addi $sp, $sp, -12            # Stack fuer 4 Eintraege reservieren
+    addi $sp, $sp, -16            # Stack fuer 4 Eintraege reservieren
+    sw $t4, 12($sp)		  # $t4,
     sw $a0, 8($sp)                # $a0,
     sw $a1, 4($sp)                # $a1 und
     sw $ra, 0($sp)                # $ra auf dem Stack sichern
 
+    move $a0, $t4		  # wähle x hoch 2 als neue basis
+    srl $a1, $a1, 1		  # teile exponent durch 2
+
     jal pow1
 
-    lw $a2, 0($sp)                # $a2,
+    lw $ra, 0($sp)                # $ra,
     lw $a1, 4($sp)                # $a1,
-    lw $ra, 8($sp)                # $ra wieder laden
+    lw $a0, 8($sp)                # $a0,
+    lw $t4, 12($sp)               # $t4 wieder laden
+    addi $sp, $sp, 16             # stack wiederherstellen
 
-    addi $t2, $t2, 1
-    addi $t2, $t2, 2
-    div $a1, $t2                  # i mod 2
+    addi $t2, $zero, 2		 
+    div $a1, $t2                  # mod 2, test auf gerade / ungerade
     mfhi $t3                      # temp for the mod
 
-    bne $t3, $t1, skip_multiply_pow
-    mult $v0, $a0
+    addi $t1, $zero, 1
+    bne $t3, $t1, skip_multiply_pow	# wenn ungerade, multipliziere nochmal mit x
+    move $a1, $a0			# ursprüngliche basis
+    move $a0, $v0			# e
+    
+    addi $sp, $sp, -4            # Stack fuer 1 Eintrag reservieren
+    sw $ra, 0($sp)		  # $ra
+    
+    jal multiply		  # errechne e * x
+    
+    lw $ra, 0($sp)               # $ra
+    addi $sp, $sp, 4             # stack wiederherstellen
 
-    skip_multiply_pow:
+    skip_multiply_pow:			# ausgabe
     jr $ra
 
 ################################################################################
